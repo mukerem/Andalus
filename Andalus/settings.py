8faee0ef7a63d86bf5098c93600e11d7449d9bf9
Mukerem Ali <mukeremali112@gmail.com>
2019-07-30 21:54:07 +0300

    first

--- Andalus/settings.py
+++ Andalus/settings.py
@@ -27,9 +27,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rapiandelus@gmail.com'
+EMAIL_HOST_USER = 'youremai;@gmail.com'
 # EMAIL_HOST_PASSWORD = os.environ['DJANGO_EMAIL_PASSWORD']
-EMAIL_HOST_PASSWORD = 'adnan@bn'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
